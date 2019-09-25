1839654c26d9d0f68549f985c842934663314f55
tiico <ockelberg@gmail.com>
2019-09-25 11:47:59 +0200

    test

--- server/router/api/recipe.js
+++ server/router/api/recipe.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const recipeservice = require('../../integration/recipe-services');
 const router = express.Router();
-const config = require('../../config');
 
 /**
  * GET: a single users basic information
