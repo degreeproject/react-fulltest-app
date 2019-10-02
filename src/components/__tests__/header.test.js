9253865b21ad403a9ba15d79c0c4b1f24bbfbe69
tiico <ockelberg@gmail.com>
2019-10-02 11:48:41 +0200

    fixed test

--- src/components/__tests__/header.test.js
+++ src/components/__tests__/header.test.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom'
+import { Provider } from 'react-redux'
 import Header from '../header/header';
+import configureStore from '../../store/configureStore'
+
+const store = configureStore();
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
-  <BrowserRouter>
-    <Header />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Header />
+    </BrowserRouter>
+  </Provider>
   , div);
   ReactDOM.unmountComponentAtNode(div);
 });
